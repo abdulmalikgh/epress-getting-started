62f9f6b6a31b47cf1c6aa909c6a62c3de5c43e66
abdulmalikgh <musah.abdulmalikgh@gmail.com>
2020-07-09 11:11:21 -1200

    Add pug

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('heya malik, welcome to epress js   ')
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+app.get('/', (req,res) => {
+    res.send('hello world')
+})
+
+
+app.listen(port, () => console.log('Your app is list en app ', port))
